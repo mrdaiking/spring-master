69ca614a47f00322632194aae60804d617c32571
CuongNguyen <>
2024-04-18 08:46:30 +0900

    create service

---
+++ src/main/java/com/appsdeveloperblog/app/ws/mobileappws/service/impl/UserServiceImpl.java
@@ -0,0 +1,14 @@
+package com.appsdeveloperblog.app.ws.mobileappws.service.impl;
+
+import com.appsdeveloperblog.app.ws.mobileappws.service.UserService;
+import com.appsdeveloperblog.app.ws.mobileappws.shared.dto.UserDto;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Override
+    public UserDto createUser(UserDto user) {
+        return null;
+    }
+}
--- src/main/java/com/appsdeveloperblog/app/ws/mobileappws/ui/controller/UserController.java
+++ src/main/java/com/appsdeveloperblog/app/ws/mobileappws/ui/controller/UserController.java
@@ -1,15 +1,31 @@
 package com.appsdeveloperblog.app.ws.mobileappws.ui.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.appsdeveloperblog.app.ws.mobileappws.service.UserService;
+import com.appsdeveloperblog.app.ws.mobileappws.shared.dto.UserDto;
+import com.appsdeveloperblog.app.ws.mobileappws.ui.model.request.UserDetailsRequestModel;
+import com.appsdeveloperblog.app.ws.mobileappws.ui.model.response.UserRest;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("users")
 public class UserController {
+    @Autowired
+    UserService userService;
     @GetMapping
     public String getUser()
     {
         return "get ";
+    }
+
+    @PostMapping
+    public UserRest createUser(@RequestBody UserDetailsRequestModel userDetails) {
+            UserRest returnValue = new UserRest();
+            UserDto userDto = new UserDto();
+            BeanUtils.copyProperties(userDetails, userDto);
+            UserDto createdUser = userService.createUser(userDto);
+            BeanUtils.copyProperties(createdUser, returnValue);
+            return returnValue;
     }
 }
